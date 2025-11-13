eb532353a234a09d1db90b88235b606046699a5c
koued2-a11y <koued2@gmail.com>
2025-11-13 17:35:12 +0100

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['via.placeholder.com', 'localhost'],
+    domains: ['via.placeholder.com', 'https://streambox-1m7t.onrender.com'],
   },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://streambox-1m7t.onrender.com',
